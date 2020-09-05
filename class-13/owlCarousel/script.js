29b00d8d49d7d7d8245b0d5c406b46bf283ba06f
Tahjiba Amin <amin.tahjiba@gmail.com>
2020-09-05 23:39:55 +0600

    added class-13

---
+++ class-13/owlCarousel/script.js
@@ -0,0 +1,14 @@
+$('.slider').owlCarousel({
+    items: 1,
+    autoPlay: 2000,
+    navigation: true,
+    transitionStyle: 'fadeUp',
+    navigationText: ['<i class="fa fa-arrow-left"></i>', '<i class="fa fa-arrow-righ t"></i>'],
+    paginationNumbers: true,
+    stopOnHover: true,
+    itemsDesktop: 1,
+    itemsDesktopSmall: 1,
+    itemsTablet: 1,
+    itemsMobile: 1
+
+});
